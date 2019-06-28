16bfed68f8d2b51317d9940ff1bb8beb99a62014
Suraj <gsnv.suraj@gmail.com>
2019-06-28 19:23:43 +0530

    file system modify

--- message.php
+++ message.php
@@ -2,9 +2,9 @@
 	use PHPMailer\PHPMailer\PHPMailer;
 	use PHPMailer\PHPMailer\Exception;
 
-	require 'PHPMailer/src/Exception.php';
-	require 'PHPMailer/src/PHPMailer.php';
-	require 'PHPMailer/src/SMTP.php';
+	require 'lib/PHPMailer/src/Exception.php';
+	require 'lib/PHPMailer/src/PHPMailer.php';
+	require 'lib/PHPMailer/src/SMTP.php';
 
 	$mail = new PHPMailer(true);
 
